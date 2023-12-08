d76a512bfd4fe7cab2ba9a03c912f03c65e59a10
Zekechan11 <145641228+Zekechan11@users.noreply.github.com>
2023-12-08 21:18:20 +0800

    category & footer

--- category.php
+++ category.php
@@ -29,7 +29,7 @@ require_once('function/update.php');
         </div>
         <div class="card border-0 text-center">
             <table id="example" class=" display" style="width:100%;">
-                <thead style="position: sticky; top: 0; background-color: white; z-index: 1;">
+                <thead>
                     <tr>
                         <th class="text-center">Category Id</th>
                         <th class="text-center">Category Name</th>
