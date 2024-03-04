1d4b57972003e3de269d4309ce49a6fed0a8953b
EdisonP <133362311+edions@users.noreply.github.com>
2024-03-05 03:45:59 +0800

    misc

--- modal/process_transaction.php
+++ modal/process_transaction.php
@@ -9,28 +9,32 @@
             </div>
             <div class="modal-body">
                 <form method='post' action='' enctype='multipart/form-data'>
-                    <div class="col-md-1" style="position: relative;left:1000px;">
-                        <label for="text" class="form-label">Voucher :</label>
+                    <div class="">
+                        <label for="text" class="form-label">Voucher:</label>
                         <select class="form-select" name="edit_category_name" id="edit_category_name">
-                            <option selected disabled value=""></option>
-                            <option>10%</option>
-                            <option>30%</option>
-                            <option>50%</option>
+                            <option selected value="0">None</option>
+                            <option value="10">10%</option>
+                            <option value="30">30%</option>
+                            <option value="50">50%</option>
                         </select>
                     </div>
                     <div class="col">
-                    <div class="col-md-3">
-                        <label for="text" class="form-label">Cash :</label>
-                        <input type="text" placeholder="₱ 00.0">
-                    </div>
-                    <div class="col-md-3">
-                        <label for="text" class="form-label">Total :</label>
-                        <input type="text" placeholder="₱ 00.0">
-                    </div>
-                    <div class="col-md-3">
-                        <label for="text" class="form-label">Change :</label>
-                        <input type="text" placeholder="₱ 00.0">
-                    </div>
+                        <div class="col-md-3">
+                            <label for="text" class="form-label">Sub Total :</label>
+                            <input type="text" placeholder="₱ 00.0" value="100">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="text" class="form-label">Cash :</label>
+                            <input type="text" placeholder="₱ 00.0">
+                        </div>
+                        <div class="col-md-3">
+                            <label for="text" class="form-label">Total :</label>
+                            <input type="text" placeholder="₱ 00.0" readonly>
+                        </div>
+                        <div class="col-md-3">
+                            <label for="text" class="form-label">Change :</label>
+                            <input type="text" placeholder="₱ 00.0" readonly>
+                        </div>
                     </div>
             </div>
             <div class="modal-footer">
